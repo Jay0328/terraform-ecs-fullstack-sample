2bead92a6919f59542b7d8b2170a577ab3ba2e4b
Jay Chen <gcobs135102@gmail.com>
2022-10-06 09:05:00 +0800

    test

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -5,7 +5,7 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <>
-      <NxWelcome title="client" />
+      <NxWelcome title="client!!!" />
       <div />
     </>
   );
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -8,7 +8,7 @@ import * as express from 'express';
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to server!!!' });
+  res.send({ message: 'Welcome to server!' });
 });
 
 app.get('/health', (req, res) => {
